4be7fbe1ce8a20bdcbe9187dbfd32e4cf48b14f0
Conor O'Brien <conorepobrien@gmail.com>
2023-01-14 15:22:50 -0800

    added package for dynamic html views. so far only have code for creating new plan

---
+++ views/home.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file contains the code for the "home" view.
+ * @version 1.0
+ * @author Conor O'Brien
+ */
+
---
+++ views/new_plan.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file contains the code for the "new plan" view.
+ * It utilizes PHP to dynamically generate HTML output for the planning view.
+ * @version 1.0
+ * @author Conor O'Brien
+ */
+
+$quartersArr = array("Fall", "Winter", "Spring", "Summer");
+
+echo "        
+        <form id='scheduleSubmit' method='post'>
+            <div class='container-fluid'>
+                <div class='row justify-content-center'>";
+
+foreach ($quartersArr as $quarter)
+{
+    echo "                    
+                    <div class='col-10 col-md-5 shadow quarter-box m-2 p-2'>
+                        <h2>" . $quarter . "</h2>
+                        <label for='" . $quarter . "'>CLASSES</label>
+                        <textarea class='quarterInput form-control' id='" . $quarter . "' rows='5'></textarea>
+                    </div>
+    ";
+}
+
+echo "            </div>
+            </div>
+        </form>";
