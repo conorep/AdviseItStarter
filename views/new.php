c5a2e6e7cbe9f70263b6611c1d15edc15d60096b
Conor O'Brien <conorepobrien@gmail.com>
2023-01-14 18:44:07 -0800

    re-added the 'display random code' to the 'new schedule' view

--- views/new.php
+++ views/new.php
@@ -1,11 +1,14 @@
 <?php
-
+include("../model/php/db_functions.php");
 /**
  * This file contains the code for the "new plan" view.
  * It utilizes PHP to dynamically generate HTML output for the planning view.
  * @version 1.0
  * @author Conor O'Brien
  */
+$databaseFunctions = new DBFunctions();
+/*TODO: generateUniqueID should be tied to a new plan on-click event*/
+$databaseFunctions->generateUniqueID();
 
 $quartersArr = array("Fall", "Winter", "Spring", "Summer");
 
