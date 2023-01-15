f21c09815802d823ed79154b20210d9b41e924eb
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 13:48:39 -0800

    added requirement for both existing classes in model and controller packages. now instantiating each class here and calling token generation display function in controller. also added submit button, which does nothing yet.

--- views/new.php
+++ views/new.php
@@ -1,33 +1,42 @@
 <?php
-include("../model/php/db_functions.php");
-/**
- * This file contains the code for the "new plan" view.
- * It utilizes PHP to dynamically generate HTML output for the planning view.
- * @version 1.0
- * @author Conor O'Brien
- */
-$databaseFunctions = new DBFunctions();
-/*TODO: generateUniqueID should be tied to a new plan on-click event*/
-$databaseFunctions->generateUniqueID();
+    require("../controller/php/model_calls.php");
+    require("../model/php/db_functions.php");
+    /**
+     * This file contains the code for the "new plan" view.
+     * It utilizes PHP to dynamically generate HTML output for the planning view.
+     * @version 1.0
+     * @author Conor O'Brien
+     */
+    $dbFunctions = new DBFunctions();
+    $modelCalls = new ModelCalls($dbFunctions);
+    $quartersArr = array("Fall", "Winter", "Spring", "Summer");
 
-$quartersArr = array("Fall", "Winter", "Spring", "Summer");
 
-echo "        
+    echo "        
         <form id='scheduleSubmit' method='post'>
             <div class='container-fluid'>
                 <div class='row justify-content-center'>";
 
-foreach ($quartersArr as $quarter)
-{
-    echo "                    
+    /*call the unique token generator method*/
+    $modelCalls->displayUniqueToken();
+
+    foreach ($quartersArr as $quarter)
+    {
+        echo "                    
                     <div class='col-10 col-md-5 shadow quarter-box m-2 p-2'>
                         <h2>" . $quarter . "</h2>
                         <label for='" . $quarter . "'>CLASSES</label>
                         <textarea class='quarterInput form-control' id='" . $quarter . "' rows='5'></textarea>
                     </div>
     ";
-}
+    }
 
-echo "            </div>
+    echo "
+                    <button id='submit-schedule-button' class='scheduleButton'>
+                        SUBMIT
+                    </button>
+    ";
+
+    echo "            </div>
             </div>
         </form>";
