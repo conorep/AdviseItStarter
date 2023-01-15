674e7351eb2749e61f71cefda21447ce9de46516
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 13:46:51 -0800

    ran a code reformat, which fixed styling issues. changed display of home page info and list.

--- views/home.php
+++ views/home.php
@@ -1,24 +1,24 @@
 <?php
 
-/**
- * This file contains the code for the "home" view.
- * @version 1.0
- * @author Conor O'Brien
- */
+    /**
+     * This file contains the code for the "home" view.
+     * @version 1.0
+     * @author Conor O'Brien
+     */
 
-echo "
+    echo "
 
-        <h1 class='text-center'>HOME PAGE</h1>
-
+        <h1 class='text-center text-decoration-underline'>HOME PAGE</h1>
+        <br/>
         <div class='container-fluid row justify-content-center'>
             <h3 class='text-center'>Hello advisor!</h3>
 
             <div class='col-6 mb-2'>
-                This is your home page. Please click a button to select what you're here for:
-                <ul>
-                    <li><em>Home:</em> You're here.</li>
-                    <li><em>New Schedule:</em> Create a new schedule plan.</li>
-                    <li><em>Retrieve Schedule:</em> View an existing plan.</li>
+                <p class='text-center'>This is your home page. Please click a button to select what you're here for:</p>
+                <ul class='list-group list-group-flush'>
+                    <li class='list-group-item'><em>Home:</em> You're here.</li>
+                    <li class='list-group-item'><em>New Schedule:</em> Create a new schedule plan.</li>
+                    <li class='list-group-item'><em>Retrieve Schedule:</em> View an existing plan.</li>
                 </ul>
             </div>
 
