18015202d9dbec51919f6788a63a8d25ac07ba01
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 13:43:57 -0800

    added TODO mentioning that this file is unneeded. also ran code reformat which fixed several style problems.

--- model/php/db_connection.php
+++ model/php/db_connection.php
@@ -1,35 +1,35 @@
 <?php
+    /*TODO: get rid of this file. was used for testing only.*/
+    /*THIS FILE WILL BE STORED SECURELY ON THE SERVER*/
+    require("connection_info.php");
 
-/*THIS FILE WILL BE STORED SECURELY ON THE SERVER*/
-require("connection_info.php");
+    /*create connection*/
+    $connection = new mysqli($dbhost, $dbuser, $dbpass, $db);
 
-/*create connection*/
-$connection = new mysqli($dbhost, $dbuser, $dbpass, $db);
-
-if($connection -> connect_error)
-{
-    die("Database connection failed: " . $connection->connect_error);
-} else
-{
-    echo " Connection successful!\n";
-
-    $selectAll = "SELECT * FROM schedules";
-    $result = mysqli_query($connection, $selectAll);
-    if(mysqli_num_rows($result) > 0)
+    if ($connection->connect_error)
     {
-        while($row = mysqli_fetch_assoc($result))
-        {
-            echo " Schedule ID: " . $row["scheduleID"];
-        }
+        die("Database connection failed: " . $connection->connect_error);
     } else
     {
-        echo " No results from select query.";
-    }
-    /*this isn't necessary in a program of this size. close does the trick. not stressing about memory leak or anything.*/
-    //mysqli_free_result($result);
+        echo " Connection successful!\n";
 
-    /*close connection when done (for now)*/
-    $connection->close();
-}
+        $selectAll = "SELECT * FROM schedules";
+        $result = mysqli_query($connection, $selectAll);
+        if (mysqli_num_rows($result) > 0)
+        {
+            while ($row = mysqli_fetch_assoc($result))
+            {
+                echo " Schedule ID: " . $row["scheduleID"];
+            }
+        } else
+        {
+            echo " No results from select query.";
+        }
+        /*this isn't necessary in a program of this size. close does the trick. not stressing about memory leak or anything.*/
+        //mysqli_free_result($result);
+
+        /*close connection when done (for now)*/
+        $connection->close();
+    }
 
 
