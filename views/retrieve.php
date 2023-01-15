53a5eb79487cafa2b7f1e2b76cd577e2b890e6dc
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 13:49:07 -0800

    ran code reformat, which fixed spacing for the document description header.

--- views/retrieve.php
+++ views/retrieve.php
@@ -1,7 +1,7 @@
 <?php
 
-/**
- * This file contains the code for the "retrieve" view.
- * @version 1.0
- * @author Conor O'Brien
- */
+    /**
+     * This file contains the code for the "retrieve" view.
+     * @version 1.0
+     * @author Conor O'Brien
+     */
