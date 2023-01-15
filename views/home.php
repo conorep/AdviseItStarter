61dadebff673f25c4ea19961fe66d453434183f8
Conor O'Brien <conorepobrien@gmail.com>
2023-01-14 17:14:38 -0800

    upgated home view with code that was in index during earlier dev. added title and file info to retrieve.

--- views/home.php
+++ views/home.php
@@ -6,3 +6,22 @@
  * @author Conor O'Brien
  */
 
+echo "
+
+        <h1 class='text-center'>HOME PAGE</h1>
+
+        <div class='container-fluid row justify-content-center'>
+            <h3 class='text-center'>Hello advisor!</h3>
+
+            <div class='col-6 mb-2'>
+                This is your home page. Please click a button to select what you're here for:
+                <ul>
+                    <li><em>Home:</em> You're here.</li>
+                    <li><em>New Schedule:</em> Create a new schedule plan.</li>
+                    <li><em>Retrieve Schedule:</em> View an existing plan.</li>
+                </ul>
+            </div>
+
+        </div>
+
+";
---
+++ views/retrieve.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * This file contains the code for the "retrieve" view.
+ * @version 1.0
+ * @author Conor O'Brien
+ */
