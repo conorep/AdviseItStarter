23ac61bf0d05fc3a89fe792f716169b9ae1626d1
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 19:06:53 -0800

    updated new view. now running a session and submitting properly with the help of ajax and php

--- views/new.php
+++ views/new.php
@@ -1,19 +1,28 @@
 <?php
-    require("../controller/php/model_calls.php");
-    require("../model/php/db_functions.php");
+    session_start();
+
     /**
      * This file contains the code for the "new plan" view.
      * It utilizes PHP to dynamically generate HTML output for the planning view.
      * @version 1.0
      * @author Conor O'Brien
      */
+
+    require("../controller/php/model_calls.php");
+    require("../model/php/db_functions.php");
+
+    /*create objects*/
     $dbFunctions = new DBFunctions();
     $modelCalls = new ModelCalls($dbFunctions);
+//    $_SESSION['modelCalls'] = serialize($modelCalls);
+
+    /*make the objects usable in schedule_submit.php*/
+//    include("../controller/php/schedule_submit.php");
+
     $quartersArr = array("Fall", "Winter", "Spring", "Summer");
 
-
     echo "        
-        <form id='scheduleSubmit' method='post'>
+        <form id='scheduleSubmit' action='' method='post'>
             <div class='container-fluid'>
                 <div class='row justify-content-center'>";
 
@@ -26,13 +35,13 @@
                     <div class='col-10 col-md-5 shadow quarter-box m-2 p-2'>
                         <h2>" . $quarter . "</h2>
                         <label for='" . $quarter . "'>CLASSES</label>
-                        <textarea class='quarterInput form-control' id='" . $quarter . "' rows='5'></textarea>
+                        <textarea class='quarterInput form-control' id='" . $quarter . "' rows='5' name='" . $quarter . "'></textarea>
                     </div>
     ";
     }
 
     echo "
-                    <button id='submit-schedule-button' class='scheduleButton'>
+                    <button type='submit' id='submit-schedule-button' class='scheduleButton' value='newSchedule' name='newScheduleSubmit'>
                         SUBMIT
                     </button>
     ";
