7fb456db3a0e0479c997a0a7f3e1e34a35690bf1
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 19:03:34 -0800

    continued work on model_calls controller page. now have ability to add new schedules to db

--- controller/php/model_calls.php
+++ controller/php/model_calls.php
@@ -18,6 +18,7 @@
             $this->databaseFuncs = $databaseFuncs;
         }
 
+        /*TODO: fix this up a bit. hidden input with the token is not ideal - can be edited.*/
         /**
          * This function utilizes the DB model class to retrieve  a token and display it on the page.
          *      The div is generated with the token as its ID for usage in the form submit function.
@@ -28,9 +29,36 @@
             $thisUniqueId = $this->databaseFuncs->generateUniqueID();
             echo "
                 <div id='" . $thisUniqueId . "' class='container-fluid'>
-                    <h3 class='text-center'>Schedule Token: " . $thisUniqueId . "</h3>
+                    <h3 class='text-center' >Schedule Token: " . $thisUniqueId . "</h3>
+                    <input name='UniqueID' class='d-none' value=" . $thisUniqueId . " />
                 </div>
             ";
+        }
+
+        /**
+         * This function retrieves a plan and displays it on the page.
+         * @return void
+         */
+        function displayCreatedPlan()
+        {
+
+        }
+
+        /**
+         * This function creates a new plan.
+         * @return bool|mysqli_result
+         */
+        function createNewPlan($id, $fall, $winter, $spring, $summer)
+        {
+            //TODO: validation here! PRE db access.
+            //$this->databaseFuncs->createNewTest();
+            return $this->databaseFuncs->createNewSchedule($id, $fall, $winter, $spring, $summer);
+        }
+
+        /*THIS IS FOR TESTING THAT DIFFERENT VIEWS CAN ACCESS A CREATED OBJECT*/
+        function testingStuff()
+        {
+            return "hi, you've reached the object";
         }
 
     }
