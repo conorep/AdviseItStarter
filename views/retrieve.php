9e89bf1c35d114e4fb74f4f161dd808409b1b00b
O'Brien <Conor.OBrien@ATRO6.astronics.com>
2023-01-16 13:31:57 -0800

    added some includes in retrieve view. now using a session variable to check what view is active in include file (schedule_form.php)

--- views/retrieve.php
+++ views/retrieve.php
@@ -3,8 +3,13 @@
 
     /**
      * This file contains the code for the "retrieve" view.
+     * It utilizes PHP to dynamically generate HTML output for the planning view
+     *  and retrieves existing schedule data from the DB.
      * @version 1.0
      * @author Conor O'Brien
      */
 
-    echo " here's some stuff ";
+    include('../include/object_creation.php');
+    $_SESSION['pageID'] = "RetrieveView";
+
+    include('../include/schedule_form.php');
