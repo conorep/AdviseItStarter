b15564bd03898fb70341473e0cfa6c8b253e4ad8
O'Brien <Conor.OBrien@ATRO6.astronics.com>
2023-01-16 13:31:04 -0800

    moved all of the PHP HTML generation to an include file and replaced that with a couple of includes

--- views/new.php
+++ views/new.php
@@ -8,44 +8,7 @@
      * @author Conor O'Brien
      */
 
-    require("../controller/php/model_calls.php");
-    require("../model/php/db_functions.php");
+    include('../include/object_creation.php');
+    $_SESSION['pageID'] = "NewView";
 
-    /*create objects*/
-    $dbFunctions = new DBFunctions();
-    $modelCalls = new ModelCalls($dbFunctions);
-//    $_SESSION['modelCalls'] = serialize($modelCalls);
-
-    /*make the objects usable in schedule_submit.php*/
-//    include("../controller/php/schedule_submit.php");
-
-    $quartersArr = array("Fall", "Winter", "Spring", "Summer");
-
-    echo "        
-        <form id='scheduleSubmit' action='' method='post'>
-            <div class='container-fluid'>
-                <div class='row justify-content-center'>";
-
-    /*call the unique token generator method*/
-    $modelCalls->displayUniqueToken();
-
-    foreach ($quartersArr as $quarter)
-    {
-        echo "                    
-                    <div class='col-10 col-md-5 shadow quarter-box m-2 p-2'>
-                        <h2>" . $quarter . "</h2>
-                        <label for='" . $quarter . "'>CLASSES</label>
-                        <textarea class='quarterInput form-control' id='" . $quarter . "' rows='5' name='" . $quarter . "'></textarea>
-                    </div>
-    ";
-    }
-
-    echo "
-                    <button type='submit' id='submit-schedule-button' class='scheduleButton' value='newSchedule' name='newScheduleSubmit'>
-                        SUBMIT
-                    </button>
-    ";
-
-    echo "            </div>
-            </div>
-        </form>";
+    include('../include/schedule_form.php');
