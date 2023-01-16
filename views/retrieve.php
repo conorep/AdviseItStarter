3736ceb36849e78c63766fc6bda4c50c6ac7af06
Conor O'Brien <conorepobrien@gmail.com>
2023-01-15 19:07:13 -0800

    added a tester echo to know it's real

--- views/retrieve.php
+++ views/retrieve.php
@@ -1,7 +1,10 @@
 <?php
+    session_start();
 
     /**
      * This file contains the code for the "retrieve" view.
      * @version 1.0
      * @author Conor O'Brien
      */
+
+    echo " here's some stuff ";
