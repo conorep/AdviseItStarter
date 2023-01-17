fb23cb1d3a17367d172662418f8e551b00596241
Conor O'Brien <conorepobrien@gmail.com>
2023-01-17 00:27:38 -0800

    moved form opening HTML to its own file

---
+++ include/form_opening.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+
+    /**
+     * This file contains the HTML for the opening of the form block.
+     * @version 1.0
+     * @author Conor O'Brien
+     */
+
+    echo "        
+            <form id='scheduleSubmit' action='' method='post'>
+                <div class='container-fluid'>
+                    <div class='row justify-content-center'>";
