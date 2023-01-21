f5337ab04c8249bc9afc940aeb3d8b89fceb2431
Conor O'Brien <91850829+conorep@users.noreply.github.com>
2023-01-20 17:36:17 -0800

    Delete connection_info.php

--- model/connection_info.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    /*TODO: MOVE THIS PRIVATE DATA TO ROOT ON SERVER*/
-    /*connection info*/
-    $dbhost = "localhost";
-    $dbuser = "cobrieng_adviseItAdmin";
-    $dbpass = "+G88-k*{G~Vg";
-    $db = "cobrieng_AdviseIt";
