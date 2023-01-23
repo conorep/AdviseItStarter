4139c593486d539cc2b7111eac72bcfbc0c8ebe3
Conor O'Brien <conorepobrien@gmail.com>
2023-01-22 16:49:44 -0800

    updated list item to reflect viewing all schedules. added scheduleToken handling to reroute to retrieve view if token is present.

--- views/home.php
+++ views/home.php
@@ -6,28 +6,33 @@
      * @version 1.0
      * @author Conor O'Brien
      */
-    include('../include/object_creation.php');
+
+    if(!isset($_SESSION['scheduleToken']))
+    {
+        include('../include/object_creation.php');
 //    var_dump($_SESSION);
 
-    $_SESSION['pageID'] = 'HomeView';
-    $modelCalls->unsetVars($quartersArr);
+        $_SESSION['pageID'] = 'HomeView';
+        $modelCalls->unsetVars($quartersArr);
 
-    echo "
+        echo "
 
         <h1 class='text-center text-decoration-underline'>HOME PAGE</h1>
         <br/>
         <div class='container-fluid row justify-content-center'>
-            <h3 class='text-center'>Hello, advisor!</h3>
+            <h3 class='text-center'>Welcome!</h3>
 
             <div class='col-6 mb-2'>
                 <p class='text-center'>This is your home page. Please click a button to select what you're here for:</p>
                 <ul class='list-group list-group-flush'>
-                    <li class='list-group-item'><em>Home:</em> You're here.</li>
-                    <li class='list-group-item'><em>New Schedule:</em> Create a new schedule plan.</li>
-                    <li class='list-group-item'><em>Retrieve Schedule:</em> View an existing plan.</li>
+                    <li class='list-group-item'><em>HOME:</em> You're here.</li>
+                    <li class='list-group-item'><em>NEW SCHEDULE:</em> Create a new schedule plan.</li>
+                    <li class='list-group-item'><em>VIEW SCHEDULES:</em> View all existing plans.</li>
                 </ul>
             </div>
 
-        </div>
-
-";
+        </div>";
+    } else
+    {
+        include('retrieve.php');
+    }
