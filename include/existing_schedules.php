17456489247c4ea1898c8919bd7f6ff2298c4ad8
Conor O'Brien <conorepobrien@gmail.com>
2023-01-22 16:42:47 -0800

    updated existing schedules to include an input form for advisor name

--- include/existing_schedules.php
+++ include/existing_schedules.php
@@ -2,7 +2,8 @@
     session_start();
 
     /**
-     * This file contains function calls to generate, display, and allow retrieval of existing schedules.
+     * This file contains HTML for a search box in the 'admin schedules' view and a function call to show
+     *      all existing schedules.
      * @version 1.0
      * @author Conor O'Brien
      */
@@ -11,7 +12,9 @@
             <div class='container-fluid px-0'>
                 <div class='container-fluid d-flex row justify-content-center mx-0'>
                     <div class=' col-6 mx-5 mb-3 text-center'>
-                        <label for='searchSchedules' class='form-label fw-bold'>Start typing a schedule token to narrow choices:</label>
+                        <label for='searchSchedules' class='form-label fw-bold'>
+                            Start typing a schedule token to narrow choices:
+                        </label>
                         <input id='searchSchedules' type='search' class='form-control shadow-sm' 
                                     placeholder='Search...' aria-label='Search through schedule IDs.'/>
                     </div>
