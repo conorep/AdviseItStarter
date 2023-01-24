bd9b48779b16d5588fc170c1addadfa1a93991fd
Conor O'Brien <conorepobrien@gmail.com>
2023-01-23 22:56:42 -0800

    added a login ajax calls page. post is used for login attempts, get is used for logout.

---
+++ controller/login_ajax_call.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+
+    /**
+     * This file handles the login or check login function for site administrators.
+     * @version 1.0
+     * @author Conor O'Brien
+     */
+    include('../include/object_creation.php');
+
+    /**
+     * The POST ajax call creates attempts an admin login.
+     */
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if(!empty($_POST))
+        {
+            $logEmail = $_POST['AdminEmail'];
+            $logPass = $_POST['AdminPass'];
+            if($modelCalls->adminLogin($logEmail, $logPass))
+            {
+                echo '<script>console.log("you logged in!")</script>';
+            };
+        }
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        if(!empty($_GET))
+        {
+            if($_GET['LogGet'] == 'yes')
+            {
+                $modelCalls->logout();
+            }
+        }
+    }
